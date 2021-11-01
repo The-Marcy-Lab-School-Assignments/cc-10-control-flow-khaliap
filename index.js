7c9c4f60f3262d749f72a5fe3ffcabf79440e2a1
khalia <khaliaparris01@gmail.com>
2021-11-01 14:14:18 +0000

    finished code challenge

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 //1
-function greatestOfTwo(){
-  
+function greatestOfTwo(num1, num2){
+  console.log(Math.max(num1,num2))
 }
-
+// greatestOfTwo(126,20)
 //2
-function greatestOfThree(){
-  
+function greatestOfThree(num1,num2,num3){
+  if (num1 > num2 && num1> num3){
+      return num1
+  }else if (num2 > num3){
+      return num2
+  }else {
+      return num3
+  }
 }
-
+// console.log(greatestOfThree(32,15,53))
 //3
-function greatestOfFour(){
-  
+function greatestOfFour(num1, num2, num3, num4){
+  if (num1 > num2 && num1> num3 && num1 >num4){
+      return num1
+  }else if (num2 > num3 && num2 > num4){
+      return num2
+  }else if (num3 > num4) {
+      return num3
+  }else {
+      return num4
+  }
 }
-
+// console.log(greatestOfFour(50, 22, 36, 11))
 
 //4
-function leastOfFour(){
-  
+function leastOfFour(num1, num2, num3, num4){
+if (num1 < num2 && num1 < num3 && num1 <num4){
+      return num1
+  }else if (num2 < num3 && num2 < num4){
+      return num2
+  }else if (num3 < num4) {
+      return num3
+  }else {
+      return num4
+  }
 }
+console.log(leastOfFour(-5, -2, -6, 0))
